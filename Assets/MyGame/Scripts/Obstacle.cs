0ced459db0a294b702087a6524538b988ba11a81
ThomasJunger <thomasjunger@gmx.at>
2019-12-16 13:42:12 +0100

    refactoring: increased readability in Obstacle Script

--- Assets/MyGame/Scripts/Obstacle.cs
+++ Assets/MyGame/Scripts/Obstacle.cs
@@ -2,31 +2,32 @@
 
 public class Obstacle : MonoBehaviour
 {
-private Rigidbody2D rb;
-[SerializeField] private float MoveSpeed;
+    private Rigidbody2D rb;
+    [SerializeField] private float MoveSpeed;
 
-private void Awake()
-{
-    rb = GetComponent<Rigidbody2D>();
-}
-
-// Update is called once per frame
-void Update()
-{
-    //if obstacle's position x is < -15f it will be destroyed
-    if(transform.position.x < -15f)
+    private void Awake()
     {
-        Destroy(gameObject);
-}
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         //if obstacle's position x is < -15f it will be destroyed
+        if(transform.position.x < -15f)
+        {
+            Destroy(gameObject);
+        }
+        
+         //if obstacle's position x is < -15f it will be destroyed
         if (transform.position.x > 15f)
         {
             Destroy(gameObject);
         }
-}
+    }
 
-private void FixedUpdate()
-{
-    rb.velocity = Vector2.left * MoveSpeed;
-}
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.left * MoveSpeed;
+    }
 }
